7a61785c9138883e593680b54913fe12e1da7a12
Alex Paarfus <stewie410@gmail.com>
2024-07-03 09:17:22 -0400

    Removed unlikely-to-use colorschemes

--- nvim/lua/custom/plugins/colorschemes.lua
+++ nvim/lua/custom/plugins/colorschemes.lua
@@ -1,9 +1,4 @@
 return {
   "Shatur/neovim-ayu",
-  "craftzdog/solarized-osaka.nvim",
-  { "rose-pine/neovim", name = "rose-pine" },
   "folke/tokyonight.nvim",
-  "RRethy/base16-nvim",
-  { "catppuccin/nvim", name = "catppuccin" },
-  "Mofiqul/vscode.nvim",
 }
