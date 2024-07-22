18b249f7cea8820843ade77303e67b4b0c352306
Alex Paarfus <stewie410@gmail.com>
2024-07-22 15:23:29 -0400

    Ensure syntax/lsp for Xresources files

---
+++ nvim/ftdetect/Xresources.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.Xresources", "*.Xdefaults", "*.Xmodmap" },
+  callback = function()
+    vim.opt_local.filetype = "xdefaults"
+    vim.opt_local.syntax = "xdefaults"
+  end,
+  desc = "Xdefaults",
+})
