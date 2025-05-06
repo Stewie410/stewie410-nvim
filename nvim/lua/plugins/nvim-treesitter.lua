f0c56a65d3ff36898f9a709c57ca3c187c180113
Alex Paarfus <stewie410@gmail.com>
2025-05-06 19:06:24 -0400

    fix(treesitter): simplified opts

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,99 +1,25 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false,
-    build = ":TSUpdate",
     lazy = false,
+    build = ":TSUpdate",
     init = function(plugin)
       -- https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/plugins/treesitter.lua#L22
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
     end,
-    cmd = {
-      "TSUpdateSync",
-      "TSUpdate",
-      "TSInstall",
-    },
-    opts_extend = { "ensure_installed" },
-    ---@type TSConfig
-    ---@diagnostic disable-next-line: missing-fields
     opts = {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        "awk",
-        "bash",
-        "bibtex",
-        "c",
-        "c_sharp",
-        "cmake",
-        -- "comment",
-        "cpp",
-        "css",
-        "diff",
-        "dockerfile",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "go",
-        "gpg",
-        "graphql",
-        "groovy",
-        "html",
-        "hyprlang",
-        "ini",
-        "javascript",
-        "jq",
-        "jsdoc",
-        "json",
-        "jsonc",
-        "latex",
-        "lua",
-        "luadoc",
-        "luap",
-        "luau",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "nginx",
-        "nix",
-        "passwd",
-        "powershell",
-        "printf",
-        "properties",
-        "psv",
-        "python",
-        "query",
-        "readline",
-        "regex",
-        "sql",
-        "ssh_config",
-        "terraform",
-        "tmux",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-        "zig",
-      },
+      indent = { enable = true },
+      ensure_installed = "all",
       auto_install = true,
     },
     config = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        opts.ensure_installed =
-          require("util.list").dedup(opts.ensure_installed)
-      end
-      require("nvim-treesitter.configs").setup(opts)
+      local configs = require("nvim-treesitter.configs")
+      configs.setup(opts)
     end,
   },
 }
