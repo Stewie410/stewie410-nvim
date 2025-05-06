9a4e542767f81326e1793742f191ac2e5b078091
Alex Paarfus <stewie410@gmail.com>
2025-05-06 10:24:35 -0400

    fix(yank): vim.highlight deprecated, switch to vim.hl

--- nvim/plugin/yank.lua
+++ nvim/plugin/yank.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   pattern = { "*" },
   group = vim.api.nvim_create_augroup("yank-highlight", { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = "150" })
+    vim.hl.on_yank({ higroup = "IncSearch", timeout = "150" })
   end,
   desc = "Highlight text on yank",
 })
