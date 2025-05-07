9682c4a6ff1441cd138b06fcdf9cc57055e1e881
Alex Paarfus <stewie410@gmail.com>
2025-05-07 10:04:11 -0400

    fix(mason): mason v2 released breaking changes; pin to v1.x

--- nvim/lua/plugins/mason-tool-installer.lua
+++ nvim/lua/plugins/mason-tool-installer.lua
@@ -1,12 +1,23 @@
 local available = require("util.lsp.servers").available()
 
+-- TODO write my own tool-installer, based on mason v2.x
 return {
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     lazy = false,
     dependencies = {
-      { "williamboman/mason.nvim",           lazy = false, opts = {} },
-      { "williamboman/mason-lspconfig.nvim", lazy = false, opts = {} },
+      {
+        "mason-org/mason.nvim",
+        version = "1.x",
+        lazy = false,
+        opts = {},
+      },
+      {
+        "mason-org/mason-lspconfig.nvim",
+        version = "1.x",
+        lazy = false,
+        opts = {},
+      },
     },
     opts = {
       ensure_installed = available,
