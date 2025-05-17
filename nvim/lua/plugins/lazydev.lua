f94bba855eb02273fa50934737f92906222907c4
Alex Paarfus <stewie410@gmail.com>
2025-05-17 13:54:59 -0400

    feat(lazydev): wezterm type support

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -2,9 +2,13 @@ return {
   {
     "folke/lazydev.nvim",
     ft = "lua",
+    dependencies = {
+      "gonstoll/wezterm-types",
+    },
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        { path = "wezterm-types", mods = { "wezterm" } },
       },
     },
   },
