6f870fe11c1fd8fb974f7c949fe419be36d8325d
Alex Paarfus <stewie410@gmail.com>
2025-09-03 19:24:12 -0400

    chore(ux): simplify colorscheme setter

--- nvim/plugin/colorscheme.lua
+++ nvim/plugin/colorscheme.lua
@@ -1,11 +1,4 @@
-local pref = "ayu"
-local fb = "default"
-
-local status_ok, _ = pcall(vim.cmd.colorscheme, pref)
-if not status_ok then
-  vim.notify(
-    'Missing scheme "' .. pref .. '"\nFalling back to "' .. fb .. '"',
-    vim.log.levels.WARN
-  )
-  vim.cmd.colorscheme(fb)
+local ok, _ = pcall(vim.cmd.colorscheme, "ayu")
+if not ok then
+  vim.cmd.colorscheme("default")
 end
