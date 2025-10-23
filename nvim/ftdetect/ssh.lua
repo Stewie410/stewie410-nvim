8c63fdfd2285221d808bf100d446805a2c025ce7
Alex Paarfus <stewie410@gmail.com>
2025-10-23 13:14:19 -0400

    feat(ftdetect): ensure system ssh(d) configs are highlighted

--- nvim/ftdetect/ssh.lua
+++ nvim/ftdetect/ssh.lua
@@ -1,9 +1,20 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = {
     ".ssh/config.d/*.conf",
+    "/etc/ssh/ssh_config.d/*.conf",
   },
   callback = function()
     vim.opt_local.filetype = "sshconfig"
     vim.opt_local.syntax = "sshconfig"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = {
+    "/etc/ssh/sshd_config.d/*.conf",
+  },
+  callback = function()
+    vim.opt_local.filetype = "sshdconfig"
+    vim.opt_local.syntax = "sshdconfig"
   end,
 })
