0d1d9f27c4e47985f44a44106ff03b3929259fe4
Alex Paarfus <stewie410@gmail.com>
2025-12-18 06:33:24 -0500

    fix(ts): stick with master/legacy branch for the time being

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,6 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
+    branch = "master",
     init = function(plugin)
       -- https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/plugins/treesitter.lua#L22
       require("lazy.core.loader").add_to_rtp(plugin)
