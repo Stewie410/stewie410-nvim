ced7061fa6c56d6ee5543004a9e4a7508fb09830
Alex Paarfus <stewie410@gmail.com>
2026-04-01 09:02:59 -0400

    feat(vim.pack): convert chezmoi.vim to vim.pack format

--- nvim/lua/plugins/chezmoi.lua
+++ nvim/lua/plugins/chezmoi.lua
@@ -1,10 +1,5 @@
-return {
-  {
-    -- TODO: find a lua port (or port it yourself)
-    "alker0/chezmoi.vim",
-    lazy = false,
-    init = function()
-      vim.g["chezmoi#use_tmp_buffer"] = true
-    end,
-  },
-}
+vim.pack.add({
+  "https://github.com/alker0/chezmoi.vim",
+})
+
+vim.g["chezmoi#use_tmp_buffer"] = true
