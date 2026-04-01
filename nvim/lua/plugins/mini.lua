abc905594c6f29477837fefd452cae9ff14f8caa
Alex Paarfus <stewie410@gmail.com>
2026-04-01 09:29:23 -0400

    feat(vim.pack): convert mini.nvim to vim.pack format

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,90 +1,42 @@
-return {
-  -- text editing
-  {
-    "nvim-mini/mini.ai",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.align",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.move",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.operators",
-    version = "*",
-    event = "VeryLazy",
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.pairs",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.splitjoin",
-    version = "*",
-    event = "VeryLazy",
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.surround",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
+vim.pack.add({
+  "https://github.com/nvim-mini/mini.ai",
+  "https://github.com/nvim-mini/mini.align",
+  "https://github.com/nvim-mini/mini.move",
+  "https://github.com/nvim-mini/mini.operators",
+  "https://github.com/nvim-mini/mini.pairs",
+  "https://github.com/nvim-mini/mini.splitjoin",
+  "https://github.com/nvim-mini/mini.surround",
 
-  -- general workflow
-  {
-    "nvim-mini/mini.bracketed",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-  {
-    "nvim-mini/mini.jump",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
+  "https://github.com/nvim-mini/mini.bracketed",
+  "https://github.com/nvim-mini/mini.jump",
 
-  -- appearance
-  {
-    "nvim-mini/mini.cursorword",
-    version = "*",
-    event = "VeryLazy",
-    opts = {},
+  "https://github.com/nvim-mini/mini.cursorword",
+  "https://github.com/nvim-mini/mini.hipatterns",
+  "https://github.com/nvim-mini/mini.statusline",
+})
+
+-- text editing
+require("mini.ai").setup()
+require("mini.align").setup()
+require("mini.move").setup()
+require("mini.operators").setup()
+require("mini.pairs").setup()
+require("mini.splitjoin").setup()
+require("mini.surround").setup()
+
+-- dx
+require("mini.bracketed").setup()
+require("mini.jump").setup()
+
+-- ux
+require("mini.cursorword").setup()
+require("mini.hipatterns").setup({
+  highlighters = {
+    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+    hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+    todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+    hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
   },
-  {
-    "nvim-mini/mini.hipatterns",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      local hipatterns = require("mini.hipatterns")
-      hipatterns.setup({
-        highlighters = {
-          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-          hex_color = hipatterns.gen_highlighter.hex_color(),
-        },
-      })
-    end,
-  },
-  {
-    "nvim-mini/mini.statusline",
-    version = "*",
-    lazy = false,
-    opts = {},
-  },
-}
+})
+require("mini.statusline").setup()
