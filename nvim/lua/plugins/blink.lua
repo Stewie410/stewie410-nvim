13889523c06df4b7d4a37962931064bf7ee9d8bb
Alex Paarfus <stewie410@gmail.com>
2026-04-01 08:53:58 -0400

    feat(vim.pack): convert blink.cmp to vim.pack format

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,136 +1,131 @@
-return {
-  {
-    "saghen/blink.cmp",
-    lazy = false,
-    dependencies = {
-      { "saghen/blink.compat", version = "*" },
-      "rafamadriz/friendly-snippets",
-      "mikavilpas/blink-ripgrep.nvim",
-      "folke/snacks.nvim",
+local opts = {
+  appearance = {
+    use_nvim_cmp_as_default = true,
+    nerd_font_variant = "mono",
+    kind_icons = {
+      Text = "",
+      Method = "󰆧",
+      Function = "󰊕",
+      Constructor = "",
+
+      Field = "󰇽",
+      Variable = "󰂡",
+      Property = "󰜢",
+
+      Class = "󰠱",
+      Interface = "",
+      Struct = "",
+      Module = "",
+
+      Unit = "",
+      Value = "󰎠",
+      Enum = "",
+      EnumMember = "",
+
+      Keyword = "󰌋",
+      Constant = "󰏿",
+
+      Snippet = "",
+      Color = "󰏘",
+      File = "󰈙",
+      Reference = "",
+      Folder = "󰉋",
+      Event = "",
+      Operator = "󰆕",
+      TypeParameter = "󰅲",
     },
-    version = "1.*",
-    opts = {
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "mono",
-        kind_icons = {
-          Text = "",
-          Method = "󰆧",
-          Function = "󰊕",
-          Constructor = "",
-
-          Field = "󰇽",
-          Variable = "󰂡",
-          Property = "󰜢",
-
-          Class = "󰠱",
-          Interface = "",
-          Struct = "",
-          Module = "",
-
-          Unit = "",
-          Value = "󰎠",
-          Enum = "",
-          EnumMember = "",
-
-          Keyword = "󰌋",
-          Constant = "󰏿",
-
-          Snippet = "",
-          Color = "󰏘",
-          File = "󰈙",
-          Reference = "",
-          Folder = "󰉋",
-          Event = "",
-          Operator = "󰆕",
-          TypeParameter = "󰅲",
+  },
+  completion = {
+    menu = {
+      border = "single",
+    },
+    trigger = {
+      show_on_trigger_character = true,
+    },
+  },
+  keymap = {
+    preset = "default",
+    ["<C-y>"] = {},
+    ["<Tab>"] = {
+      function(cmp)
+        if cmp.snippet_active() then
+          return cmp.accept()
+        else
+          return cmp.select_and_accept()
+        end
+      end,
+      "snippet_forward",
+      "fallback",
+    },
+  },
+  sources = {
+    default = {
+      "lazydev",
+      "lsp",
+      "path",
+      "snippets",
+      "omni",
+      "buffer",
+      "ripgrep",
+      "cmdline",
+    },
+    providers = {
+      snippets = {
+        score_offset = 50,
+        opts = {
+          search_paths = {
+            os.getenv("XDG_CONFIG_HOME") .. "/snippets",
+            os.getenv("XDG_CONFIG_HOME") .. "/private/snippets",
+            vim.fn.stdpath("data") .. "/lazy/friendly-snippets",
+            vim.fn.stdpath("config") .. "/snippets",
+          },
         },
       },
-      completion = {
-        menu = {
-          border = "single",
-        },
-        trigger = {
-          show_on_trigger_character = true,
-        },
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        score_offset = 100,
       },
-      keymap = {
-        preset = "default",
-        ["<C-y>"] = {},
-        ["<Tab>"] = {
-          function(cmp)
-            if cmp.snippet_active() then
-              return cmp.accept()
-            else
-              return cmp.select_and_accept()
-            end
-          end,
-          "snippet_forward",
-          "fallback",
-        },
-      },
-      sources = {
-        default = {
-          "lazydev",
-          "lsp",
-          "path",
-          "snippets",
-          "omni",
-          "buffer",
-          "ripgrep",
-          "cmdline",
-        },
-        providers = {
-          snippets = {
-            score_offset = 50,
-            opts = {
-              search_paths = {
-                os.getenv("XDG_CONFIG_HOME") .. "/snippets",
-                os.getenv("XDG_CONFIG_HOME") .. "/private/snippets",
-                vim.fn.stdpath("data") .. "/lazy/friendly-snippets",
-                vim.fn.stdpath("config") .. "/snippets",
-              },
+      ripgrep = {
+        name = "Ripgrep",
+        module = "blink-ripgrep",
+        opts = {
+          project_root_marker = { ".git", ".svn" },
+          backend = {
+            ripgrep = {
+              search_casing = "--smart-case",
             },
           },
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-          ripgrep = {
-            name = "Ripgrep",
-            module = "blink-ripgrep",
-            opts = {
-              project_root_marker = { ".git", ".svn" },
-              backend = {
-                ripgrep = {
-                  search_casing = "--smart-case",
-                },
-              },
-            },
-          },
-          buffer = {
-            score_offset = 0,
-          },
         },
       },
-      -- TODO:return to 'prefer_rust' at some point
-      fuzzy = { implementation = "prefer_rust" },
-      cmdline = {
-        keymap = {
-          preset = "cmdline",
-          ["<Tab>"] = { "show", "accept" },
-        },
-        completion = {
-          menu = {
-            ---@diagnostic disable-next-line: unused-local
-            auto_show = function(ctx)
-              return vim.fn.getcmdtype() == ":"
-            end,
-          },
-        },
+      buffer = {
+        score_offset = 0,
       },
     },
-    opts_extend = { "sources.default" },
+  },
+  fuzzy = { implementation = "prefer_rust" },
+  cmdline = {
+    keymap = {
+      preset = "cmdline",
+      ["<Tab>"] = { "show", "accept" },
+    },
+    completion = {
+      menu = {
+        ---@diagnostic disable-next-line: unused-local
+        auto_show = function(ctx)
+          return vim.fn.getcmdtype() == ":"
+        end,
+      },
+    },
   },
 }
+
+vim.pack.add({
+  { src = "https://github.com/saghen/blink.cmp",    version = "1.*" },
+  { src = "https://github.com/saghen/blink.compat", version = "2.*" },
+  "https://github.com/rafamdariz/friendly-snippets",
+  "https://github.com/mikavilpas/blink-ripgrep.nvim",
+  "https://github.com/folke/snacks.nvim",
+})
+
+require("blink.cmp").setup(opts)
