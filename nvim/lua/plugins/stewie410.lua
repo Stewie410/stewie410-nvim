9f823dc8f60cc09290cabb2b50a80e73922e4964
Alex Paarfus <stewie410@gmail.com>
2026-04-05 11:14:29 -0400

    feat: migrate boiler.nvim to vim.pack

--- nvim/lua/plugins/stewie410.lua
+++ nvim/lua/plugins/stewie410.lua
@@ -1,37 +1,25 @@
-return {
-  {
-    "Stewie410/boiler.nvim",
-    dev = true,
-    dependencies = {
-      "Grub4K/glib.nvim",
-      "folke/snacks.nvim",
-    },
-    cmd = {
-      "Boiler",
-      "BoilerAll",
-    },
-    keys = {
-      {
-        "<leader>bb",
-        function()
-          require("boiler").pick(vim.bo.filetype)
-        end,
-        desc = "Boiler: Select by Filetype",
-      },
-      {
-        "<leader>ba",
-        function()
-          require("boiler").pick()
-        end,
-        desc = "Boiler: Select Any",
-      },
-    },
-    opts = {
-      picker = "snacks",
-      paths = {
-        os.getenv("XDG_CONFIG_HOME") .. "/boilerplate",
-        os.getenv("XDG_CONFIG_HOME") .. "/private/boilerplate",
-      },
-    },
+vim.pack.add({
+  "https://github.com/Stewie410/boiler.nvim",
+  "https://github.com/Grub4K/glib.nvim",
+  "https://github.com/folke/snacks.nvim",
+})
+
+require("boiler").setup({
+  picker = "snacks",
+  paths = {
+    os.getenv("XDG_CONFIG_HOME") .. "/boilerplate",
+    os.getenv("XDG_CONFIG_HOME") .. "/private/boilerplate",
   },
-}
+})
+
+if not vim.g.vim_pack_init.boiler == 1 then
+  vim.keymap.set("n", "<leader>bb", function()
+    require("boiler").pick(vim.bo.filetype)
+  end, { desc = "Boiler: Select by Filetype" })
+
+  vim.keymap.set("n", "<leader>ba", function()
+    require("boiler").pick()
+  end, { desc = "Boiler: Select from Any" })
+
+  vim.g.vim_pack_init.boiler = 1
+end
