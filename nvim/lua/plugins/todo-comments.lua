2e9e2dbe27701ebfacf152955fd7093c97e61484
Alex Paarfus <stewie410@gmail.com>
2026-04-05 11:19:58 -0400

    feat: migrate todo-comments to vim.pack, lazy load

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,10 +1,13 @@
-return {
-  {
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = { signs = false },
-  },
-}
+vim.pack.add({
+  "https://github.com/folke/todo-comments.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    require("todo-comments").setup({
+      signs = false,
+    })
+  end,
+  desc = "LazyLoad: todo-comments.nvim",
+})
