bf2dc6a44a1d05bece9dfb6edbc33fe4a1847c45
Alex Paarfus <stewie410@gmail.com>
2026-04-05 11:24:28 -0400

    feat: migrate which-key to vim.pack

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,31 +1,23 @@
-return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "echasnovski/mini.icons",
-      "nvim-tree/nvim-web-devicons",
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer local keymaps",
-      },
-    },
-    opts = {
-      preset = "helix",
-      spec = {
-        { "<leader>c", group = "[C]ode" },
-        { "<leader>d", group = "[D]ocument" },
-        { "<leader>f", group = "[F]ind" },
-        { "<leader>r", group = "[R]ename" },
-        { "<leader>s", group = "[S]urround" },
-        { "<leader>w", group = "[W]orkspace" },
-        { "<leader>t", group = "[T]oggle" },
-      },
-    },
-  },
-}
+vim.pack.add({
+  "https://github.com/folke/which-key.nvim",
+  "https://github.com/echasnovski/mini.icons",
+  "https://github.com/nvim-tree/nvim-web-devicons",
+})
+
+---@diagnostic disable-next-line: missing-fields
+require("which-key").setup({
+  preset = "helix",
+  spec = {
+    { "<leader>c", group = "[C]ode" },
+    { "<leader>d", group = "[D]ocument" },
+    { "<leader>f", group = "[F]ind" },
+    { "<leader>r", group = "[R]ename" },
+    { "<leader>s", group = "[S]urround" },
+    { "<leader>w", group = "[W]orkspace" },
+    { "<leader>t", group = "[T]oggle" },
+  }
+})
+
+vim.keymap.set("n", "<leader>?", function()
+  require("which-key").show({ global = false })
+end, { desc = "Buffer local keymaps" })
