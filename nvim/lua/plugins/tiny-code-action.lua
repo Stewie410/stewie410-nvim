60833f0b840d179924fce971983f9502fcd61cc6
Alex Paarfus <stewie410@gmail.com>
2026-04-05 11:18:02 -0400

    feat: migrate tiny-code-action to vim.pack, lazy-load

--- nvim/lua/plugins/tiny-code-action.lua
+++ nvim/lua/plugins/tiny-code-action.lua
@@ -1,15 +1,14 @@
-return {
-  {
-    "rachartier/tiny-code-action.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "folke/snacks.nvim",
-    },
-    event = "LspAttach",
-    opts = {
-      picker = {
-        "snacks",
-      },
-    },
-  },
-}
+vim.pack.add({
+  "https://github.com/rachartier/tiny-code-action.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/folke/snacks.nvim",
+})
+
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+  callback = function()
+    require("tiny-code-action").setup({
+      picker = { "snacks" },
+    })
+  end,
+  desc = "Lazy-Load tiny-code-action",
+})
